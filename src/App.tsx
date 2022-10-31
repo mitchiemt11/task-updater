ee323ee21af3119b83e7b782d30520badca45b96
Mitchell <mitchiemt11@gmail.com>
2022-10-31 22:51:45 +0200

    Track user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import './App.css';
 
 const App:FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if(event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  }
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder='Task....' />
-          <input type="number" placeholder='Deadline in days....' />
+          <input
+            name="task"
+            type="text" 
+            placeholder='Task....'  
+            onChange={handleChange}
+          />
+          <input
+            name="deadline"
+            type="number"
+            placeholder='Deadline in days....'
+          />
         </div>
         <button>Add Task</button>
       </div>
